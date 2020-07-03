f478a8b034fe21057d8eb8d21e53269926107cc8
FASfalcon <alexanderfedasov@gmail.com>
2020-07-03 17:40:16 +0300

    CCA update1.1 - лишний count.

--- src/Main.java
+++ src/Main.java
@@ -11,24 +11,19 @@ public class Main {
 
         double k = 45;
         double l = 34;
-
         int d = 78;
         int h = 67;
 
         double res = Calculator.addition(d, h);
-        //int res1 = Calculator.plus(i,j);
 
         System.out.println(Calculator.division(d, h));
 
         for (int i = 0; i < 10; i++) {
-
             Counter cnt = new Counter();
-            System.out.println("getCount1: " + cnt.getCount1());
             System.out.println("getCount: " + cnt.getCount());
         }
 
         Agreement agreement = new Agreement("Agreement", new Date(), 1, List.of("bannana"));
         Act act = Adapter.toAct(agreement);
-
     }
 }
